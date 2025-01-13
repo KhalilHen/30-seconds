70827ca9983d8a9f31d9f39f6aac02aefb1a4e34
Khalil <khalilhenane@hotmail.com>
2025-01-13 12:02:30 +0100

    I was planning to use flame but i am changing
    This is the code i have, my intial plan was to do it in flame. But changed my mind to use normal flutter.

---
+++ thirty_seconds/lib/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flame/game.dart';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+
+class StartGame extends FlameGame {
+  StartGame({super.children});
+
+  @override
+  Color backgroundColor() {
+    return Colors.green;
+  }
+
+  // Text
+}
