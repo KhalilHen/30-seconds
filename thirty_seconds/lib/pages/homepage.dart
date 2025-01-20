e7c17e0d3e28e9ec0e497cd7d6239b1344ae0ced
Khalil <khalilhenane@hotmail.com>
2025-01-20 16:18:03 +0100

    Change the homepage

--- thirty_seconds/lib/pages/homepage.dart
+++ thirty_seconds/lib/pages/homepage.dart
@@ -2,7 +2,8 @@ import 'package:flame/game.dart';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-// import 'package:flutter/material.dart';
+import 'package:thirty_seconds/pages/setup_teams.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,23 +13,25 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-        Text('Welcome!',
-        style: TextStyle(
-
-
-fontSize: 25,
-fontWeight: FontWeight.bold,
-
-
-        ),),
-        // SizedBox(height:90,),
- Padding(padding: EdgeInsets.all(40)),
-        ElevatedButton(
-          onPressed: null,
-          child: Text('Start Game'),
-        ),
+            Text(
+              'Welcome!',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SetupTeamsPage()),
+                );
+              },
+              child: Text('Start Game'),
+            ),
           ],
         ),
       ),
---
+++ thirty_seconds/lib/pages/setup_teams.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SetupTeamsPage extends StatefulWidget {
+  @override
+  _SetupTeamsPageState createState() => _SetupTeamsPageState();
+}
+
+class _SetupTeamsPageState extends State<SetupTeamsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
