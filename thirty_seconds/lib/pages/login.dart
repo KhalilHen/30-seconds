78b7890d27baf4cbe0df0507b74afd3406f6078f
Khalil <khalilhenane@hotmail.com>
2025-01-27 17:22:19 +0100

    Added the 2 form fields email, and password. for login

--- thirty_seconds/lib/pages/login.dart
+++ thirty_seconds/lib/pages/login.dart
@@ -9,19 +9,103 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       backgroundColor: Colors.deepPurple[50],
       appBar: AppBar(
-        title: Text("Welcome back!", style: TextStyle(color: Colors.white),
-        
-          
+        title: Text(
+          "Welcome back!",
+          style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.deepPurple[600],
         elevation: 0,
       ),
-
       body: SingleChildScrollView(
-        // child: ,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.deepPurple[100],
+                child: Icon(
+                  Icons.person,
+                  size: 60,
+                  color: Colors.deepPurple[600],
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Colors.deepPurple[700],
+                        ),
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.deepPurple[600],
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide(color: Colors.deepPurple[200]!),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide(color: Colors.deepPurple[200]!),
+                        ),
+                        focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15), borderSide: BorderSide(color: Colors.deepPurple[600]!))),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your email";
+                      } else if (!value.contains('@') || !value.contains('.')) {
+                        return "Please enter a valid email";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(color: Colors.deepPurple[700]),
+                        prefixIcon: Icon(
+                          Icons.lock_outline,
+                          color: Colors.deepPurple[600],
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide(color: Colors.deepPurple[200]!),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide(color: Colors.deepPurple[200]!),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide(color: Colors.deepPurple[600]!),
+                        )),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your password";
+                      }
+                      return null;
+                    },
+                  )
+                ],
+              ))
+            ],
+          ),
+        ),
       ),
     );
   }
